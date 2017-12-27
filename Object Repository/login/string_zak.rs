49e0e32a940620118543f72a175e091cc4028915
zubov-skdo <30900714+skdo-testing@users.noreply.github.com>
2017-12-27 17:19:54 +0300

    login zak2

---
+++ Object Repository/login/string_zak.rs
@@ -0,0 +1,37 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>string_zak</name>
+   <tag></tag>
+   <elementGuidId>10105b65-4f68-40cc-9c53-547dc26ac52a</elementGuidId>
+   <selectorMethod>BASIC</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>tag</name>
+      <type>Main</type>
+      <value>div</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>class</name>
+      <type>Main</type>
+      <value>topbar-title</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>text</name>
+      <type>Main</type>
+      <value>Личный кабинет заказчика</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>/html[1]/body[1]/app-root[1]/div[@class=&quot;layout-wrapper&quot;]/div[@class=&quot;layout-container menu-layout-static&quot;]/app-topbar[1]/div[@class=&quot;topbar clearfix&quot;]/div[@class=&quot;topbar-right&quot;]/div[@class=&quot;topbar-title&quot;]</value>
+   </webElementProperties>
+</WebElementEntity>
