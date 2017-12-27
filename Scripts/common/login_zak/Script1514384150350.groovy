eb4a6e954999047928cafce25358e1b5dce3057a
zubov-skdo <30900714+skdo-testing@users.noreply.github.com>
2017-12-27 17:18:20 +0300

    add login zak

---
+++ Scripts/common/login_zak/Script1514384150350.groovy
@@ -0,0 +1,39 @@
+import static com.kms.katalon.core.checkpoint.CheckpointFactory.findCheckpoint
+import static com.kms.katalon.core.testcase.TestCaseFactory.findTestCase
+import static com.kms.katalon.core.testdata.TestDataFactory.findTestData
+import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
+import com.kms.katalon.core.checkpoint.Checkpoint as Checkpoint
+import com.kms.katalon.core.checkpoint.CheckpointFactory as CheckpointFactory
+import com.kms.katalon.core.mobile.keyword.MobileBuiltInKeywords as MobileBuiltInKeywords
+import com.kms.katalon.core.mobile.keyword.MobileBuiltInKeywords as Mobile
+import com.kms.katalon.core.model.FailureHandling as FailureHandling
+import com.kms.katalon.core.testcase.TestCase as TestCase
+import com.kms.katalon.core.testcase.TestCaseFactory as TestCaseFactory
+import com.kms.katalon.core.testdata.TestData as TestData
+import com.kms.katalon.core.testdata.TestDataFactory as TestDataFactory
+import com.kms.katalon.core.testobject.ObjectRepository as ObjectRepository
+import com.kms.katalon.core.testobject.TestObject as TestObject
+import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WSBuiltInKeywords
+import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
+import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUiBuiltInKeywords
+import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
+import internal.GlobalVariable as GlobalVariable
+
+WebUI.openBrowser('http://94.130.75.130')
+
+WebUI.maximizeWindow()
+
+WebUI.setText(findTestObject('login//input_add_user_login'), 'customer')
+
+WebUI.setText(findTestObject('login//input_add_user_password'), 'qQ12345678')
+
+WebUI.click(findTestObject('login/enter_btn'))
+
+WebUI.waitForElementVisible(findTestObject('login//input_confirmation_code'), 30)
+
+WebUI.setText(findTestObject('login//input_confirmation_code'), '9')
+
+WebUI.click(findTestObject('login/next_btn'))
+
+WebUI.verifyElementVisible(findTestObject('login/string_zak'))
+
